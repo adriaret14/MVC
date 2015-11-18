48d91ee64335f4bf04095b533ea58acf8e3273ca
adriaret14 <adriafret1995@gmail.com>
2015-11-18 18:44:36 +0100

    Practica 5

---
+++ apps/model.php
@@ -0,0 +1,11 @@
+<?php
+
+class model
+{
+	public function user_info()
+	{
+		return array(
+			'first'=>'Jeffrey',
+			'last'=>'Way');
+	}
+}
---
+++ views/vista.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+<h1></h1>
+<div id="header">
+<h1>Titulo</h1>
+</div>
+<div id="menu">
+</div>
+<div id="contenido">
+	<p><?= $first; ?></p>
+</div>
+<div id="footer">
+	<p>Hecho por TittoFr3ddy28</p>
+</div>
+</body>
+</html>
